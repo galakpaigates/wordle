7698a6f97db05cb56e1a397c10f363d9a684a01f
galakpaigates <jeedoarkoi2006@gmail.com>
2023-05-05 13:19:58 +0000

    Fully Completed Wordle Game, maybe unkown left...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,168 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.body.style.overflow = "hidden";
 
-    const mainBody = document.querySelector('.mainBody');
     const screensDiv = document.querySelector('.screensDiv');
-    const keysDiv = document.querySelector('.keysDiv');
-    const backspaceImg = document.querySelector('.backspaceImg');
-    backspaceImg.draggable = false;
+    const youWonH1 = document.querySelector('.youWonH1');
+    const youLostH1 = document.querySelector('.youLostH1');
+    const container = document.querySelector('.container');
+
+    var mode = "add";
+
+    var keysArray = [
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'
+    ];
+
+    var wordsArray = [
+        "apple","blend","brick","charm","clasp","clean","climb","clock","cloud","craft","crisp","croak","crown","crush","dance","dealt","death","debut","decal","delay","doubt","dream","drift","drill","drink","drive","eager","early","earth","empty","equal","equip","error","event","every","exact","fable","faith","fancy","fault","favor","fence","fetch","fever","fiber","field","fight","final","flame","flour","fluid","focus","force","found","frame","fresh","front","frost","fruit","funny","giant","glaze","globe","glory","grace","grade","grasp","green","groan","group","guard","guess"
+    ];
+
+    var luckyWord = getLuckyWord();
+
+    function getLuckyWord() {
+        return wordsArray[Math.floor(Math.random() * wordsArray.length)].toUpperCase();
+    }
+
+    function checkAlphabet(pressedKey) {
+        var result = 0;
+        
+        for (let i = 0; i < keysArray.length; i++) {
+            if (keysArray[i] === pressedKey.toLowerCase()) {
+                result += 1;
+            }
+        }
+
+        if (result === 1) {
+            return true;
+        }
+    }
+
+    function getAllScreenButtonsWithTextAndNotDisabled () {
+        
+        const allScreenDivsNodeList = getAllScreenButtons();
+        var onlyTextBtnArray = [];
+        var checkOnlyFive = 0;
+        
+        for (let i = allScreenDivsNodeList.length-1; i > -1; i--) {
+            
+            if (checkOnlyFive < 5) {
+                if (allScreenDivsNodeList[i].innerText.length > 0) {
+
+                    if (allScreenDivsNodeList[i].disabled === false) {
+                        onlyTextBtnArray += allScreenDivsNodeList[i].innerText;
+
+                        checkOnlyFive++;
+                    }
+                }
+            }
+        }
+
+        var reversedTextBtnArray = [];
+
+        for (let i = onlyTextBtnArray.length-1; i > -1; i--) {
+            reversedTextBtnArray += onlyTextBtnArray[i];
+        }
+
+        return reversedTextBtnArray;
+    }
+
+    function markIndex(currentValue, color) {
+
+        const allScreenDivsNodeList = getAllScreenButtons();
+        var checkOnlyFive = 0;
+        
+        for (let i = allScreenDivsNodeList.length-1; i > -1; i--) {
+            
+            if (checkOnlyFive < 5) {
+                if (allScreenDivsNodeList[i].innerText.length > 0) {
+                    
+                    if (allScreenDivsNodeList[i].innerText === currentValue) {
+
+                        allScreenDivsNodeList[i].style.backgroundColor = color;
+                        allScreenDivsNodeList[i].style.boxShadow = "none";
+                    }
+
+                    checkOnlyFive++;
+                }
+            }
+        }
+    }
+
+    function disableAllBtnsWithText() {
+        
+        const allScreenDivsNodeList = getAllScreenButtons();
+
+        if (getAllScreenButtonsWithTextAndNotDisabled().length === 5) {
+            for (let i = allScreenDivsNodeList.length-1; i > -1; i--) {
+                if (allScreenDivsNodeList[i].textContent.length > 0) {
+                    allScreenDivsNodeList[i].disabled = true;
+                }
+            }
+        }
+    }
+
+    function answerIsCorrect() {
+        mode = "ended";
+        container.style.opacity = "0.6";
+        container.style.pointerEvents = "none";
+
+        youWonH1.style.display = "flex"; youWonH1.style.justifyContent = "center"; youWonH1.style.alignItems = "center";
+
+        setTimeout(() => {
+            location.reload();
+        }, 8000);
+    }
+
+    function gameOver() {
+        mode = "ended";
+        container.style.opacity = "0.6";
+        container.style.pointerEvents = "none";
+
+        youLostH1.style.display = "flex"; youLostH1.style.justifyContent = "center"; youLostH1.style.alignItems = "center";
+
+        setTimeout(() => {
+            location.reload();
+        }, 3500);
+    }
+
+    function checkResult() {
+
+        const answer = getAllScreenButtonsWithTextAndNotDisabled();
+        const allScreenBtns = getAllScreenButtons();
+        
+        if (answer.length === 5) {
+
+            var correctChecks = 0;
+            var sameCharCheck = 0;
+
+            for (let i = 0; i < luckyWord.length; i++) {
+
+                if (i > 0) {
+                    if (answer[i] === answer[i-1]) {
+                        sameCharCheck++;
+                    }
+                }
+
+                if (luckyWord.includes(answer[i])) {
+                    markIndex(answer[i], "#B59F3B")
+                }
+
+                if (answer[i] === luckyWord[i]) {
+                    
+                    markIndex(answer[i], "#538D4E");
+
+                    correctChecks += 1;
+                }
+            }
+
+            if (allScreenBtns[29].innerText.length > 0) {
+                gameOver();
+            }
+
+            if (correctChecks === 5) {
+                answerIsCorrect();
+            }
+        }
+    }
 
     function insertScreenDivs() {
         for (let i = 1; i < 31; i++) {
@@ -22,20 +180,30 @@ document.addEventListener('DOMContentLoaded', () => {
         return document.querySelectorAll('.screenDiv');
     }
 
-    function addTextToScreenDiv(clickedElement) {
+    function addTextToScreen(clickedElement) {
 
-        const clickedElementTextContent = clickedElement.textContent.toUpperCase();
-        const allScreenDivsNodeList = getAllScreenButtons();
+        if (mode === "add") {
+            const clickedElementTextContent = clickedElement;
+            const allScreenDivsNodeList = getAllScreenButtons();
+            const onlyTextBtns = getAllScreenButtonsWithTextAndNotDisabled();
 
-        for (let i = 0; i < allScreenDivsNodeList.length; i++) {
+            if (onlyTextBtns.length <= 4) {
+                for (let i = 0; i < allScreenDivsNodeList.length; i++) {
 
-            if (allScreenDivsNodeList[i].innerText.length < 1) {
-                allScreenDivsNodeList[i].style.transform = "rotateY(360deg)";
-                allScreenDivsNodeList[i].style.backgroundColor = "#538D4E";
+                    if (allScreenDivsNodeList[i].innerText.length < 1) {
+                        
+                        allScreenDivsNodeList[i].style.backgroundColor = "grey";
+        
+                        allScreenDivsNodeList[i].innerText = clickedElementTextContent.toUpperCase();
 
-                allScreenDivsNodeList[i].innerText = clickedElementTextContent;
-                break
+                        break
+                    }
+                }
             }
+        }
+
+        else {
+            return;
         }
     }
 
@@ -44,27 +212,63 @@ document.addEventListener('DOMContentLoaded', () => {
         const allScreenDivsNodeList = getAllScreenButtons();
 
         for (let i = allScreenDivsNodeList.length-1; i > -1; i--) {
-                
+            
             if (allScreenDivsNodeList[i].innerText.length > 0) {
 
-                allScreenDivsNodeList[i].style.transform = "rotateY(360deg)";
-                allScreenDivsNodeList[i].style.backgroundColor = "#3A3A3C";
+                if (allScreenDivsNodeList[i].style.backgroundColor === "grey") {
 
-                allScreenDivsNodeList[i].innerText = "";
-                break
+                    if (allScreenDivsNodeList[i].disabled === false) {
+                        allScreenDivsNodeList[i].style.backgroundColor = "#3A3A3C";
+
+                        allScreenDivsNodeList[i].innerText = "";
+                        break
+                    }
+                }
+
+                else {
+                    break
+                }
             }
         }
     }
+
+    document.addEventListener('keyup', (event) => {
+        const pressedKey = event.key;
+
+        if (checkAlphabet(pressedKey) === true) {
+            addTextToScreen(pressedKey)
+        }
+
+        if (pressedKey === "Backspace") {
+            doBackspace(pressedKey)
+        }
+
+        if (pressedKey === "Enter") {
+            getAllScreenButtonsWithTextAndNotDisabled();
+
+            checkResult();
+
+            disableAllBtnsWithText();
+        }
+    })
 
     document.addEventListener('click', (event) => {
         const clickedElement = event.target;
 
         if (clickedElement.className === "key") {
-            addTextToScreenDiv(clickedElement);
+            addTextToScreen(clickedElement.textContent.toUpperCase());
         }
 
-        if (clickedElement.className === "backspaceImg") {
+        if (clickedElement.className === "backspace") {
             doBackspace(clickedElement);
+        }
+
+        if (clickedElement.className === "enter") {
+            getAllScreenButtonsWithTextAndNotDisabled();
+
+            checkResult();
+
+            disableAllBtnsWithText();
         }
     })
 })
